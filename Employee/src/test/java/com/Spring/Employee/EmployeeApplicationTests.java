a10d6798e47ab03ab79198ed6a6c4769484211e4
rahatTCS <73182854+rahatTCS@users.noreply.github.com>
2021-01-05 23:32:44 -0500

    First Commit

---
+++ Employee/src/test/java/com/Spring/Employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
